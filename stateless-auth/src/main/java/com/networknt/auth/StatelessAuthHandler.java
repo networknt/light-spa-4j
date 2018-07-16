f93d2f32cf61592c5baee7673e4e6eacdd9ebbd1
auahmed <attiqahmed92@live.ca>
2018-07-16 17:42:19 -0400

    #6 forward new jwt to service

--- stateless-auth/src/main/java/com/networknt/auth/StatelessAuthHandler.java
+++ stateless-auth/src/main/java/com/networknt/auth/StatelessAuthHandler.java
@@ -240,6 +240,7 @@ public class StatelessAuthHandler implements MiddlewareHandler {
                     return;
                 }
                 setCookies(exchange, response, csrf);
+                jwt = response.getAccessToken();
                 // now let's go to the next handler. The cookies are set for this response already.
             }
             exchange.getRequestHeaders().put(Headers.AUTHORIZATION, "Bearer " + jwt);
