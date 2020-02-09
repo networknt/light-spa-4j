157a34581ad34209533d4a2846498723491e66b1
Steve Hu <stevehu@gmail.com>
2020-02-09 09:49:45 -0500

    fixes #34 split userInfo to userId, userType and roles cookie for 1.6.x

--- stateless-auth/src/main/java/com/networknt/auth/StatelessAuthHandler.java
+++ stateless-auth/src/main/java/com/networknt/auth/StatelessAuthHandler.java
@@ -18,7 +18,6 @@ package com.networknt.auth;
 
 import com.networknt.client.oauth.*;
 import com.networknt.config.Config;
-import com.networknt.exception.ExpiredTokenException;
 import com.networknt.handler.Handler;
 import com.networknt.handler.MiddlewareHandler;
 import com.networknt.httpstring.AttachmentConstants;
@@ -26,6 +25,7 @@ import com.networknt.httpstring.HttpStringConstants;
 import com.networknt.monad.Result;
 import com.networknt.security.JwtVerifier;
 import com.networknt.status.Status;
+import com.networknt.exception.ExpiredTokenException;
 import com.networknt.utility.Constants;
 import com.networknt.utility.ModuleRegistry;
 import com.networknt.utility.Util;
@@ -42,6 +42,7 @@ import org.jose4j.jwt.consumer.InvalidJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
@@ -275,12 +276,14 @@ public class StatelessAuthHandler implements MiddlewareHandler {
         long expiresIn = response.getExpiresIn();
         // parse the access token.
         JwtClaims claims = null;
-        JSONObject userInfo = new JSONObject();
+        String[] roles = null;
+        String userType = null;
+        String userId = null;
         try {
             claims = jwtVerifier.verifyJwt(accessToken, true, true);
-            userInfo.put("roles",claims.getStringListClaimValue(Constants.ROLES_STRING).toArray(new String[0]));
-            userInfo.put("userType",claims.getStringClaimValue(Constants.USER_TYPE_STRING));
-            userInfo.put("userId",claims.getStringClaimValue(Constants.USER_ID_STRING));
+            roles = claims.getStringListClaimValue(Constants.ROLES_STRING).toArray(new String[0]);
+            userType = claims.getStringClaimValue(Constants.USER_TYPE_STRING);
+            userId = claims.getStringClaimValue(Constants.USER_ID_STRING);
         } catch (InvalidJwtException e) {
             logger.error("Exception: ", e);
             setExchangeStatus(exchange, INVALID_AUTH_TOKEN);
@@ -302,12 +305,28 @@ public class StatelessAuthHandler implements MiddlewareHandler {
                 .setHttpOnly(true)
                 .setSecure(config.cookieSecure));
         // this is user info in cookie and it is accessible for Javascript.
-        exchange.setResponseCookie(new CookieImpl("userInfo", userInfo.toString())
+        exchange.setResponseCookie(new CookieImpl("userId", userId)
                 .setDomain(config.cookieDomain)
                 .setPath(config.cookiePath)
                 .setMaxAge(config.cookieMaxAge)
                 .setHttpOnly(false)
                 .setSecure(config.cookieSecure));
+        if(userType != null) {
+            exchange.setResponseCookie(new CookieImpl("userType", userType)
+                    .setDomain(config.cookieDomain)
+                    .setPath(config.cookiePath)
+                    .setMaxAge(config.cookieMaxAge)
+                    .setHttpOnly(false)
+                    .setSecure(config.cookieSecure));
+        }
+        if(roles != null && roles.length > 0) {
+            exchange.setResponseCookie(new CookieImpl("roles", Arrays.toString(roles))
+                    .setDomain(config.cookieDomain)
+                    .setPath(config.cookiePath)
+                    .setMaxAge(config.cookieMaxAge)
+                    .setHttpOnly(false)
+                    .setSecure(config.cookieSecure));
+        }
         // this is another csrf token in cookie and it is accessible for Javascript.
         exchange.setResponseCookie(new CookieImpl(Constants.CSRF_STRING, csrf)
                 .setDomain(config.cookieDomain)
