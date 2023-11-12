be013a9fdd58a0ca2625ed97167aee1c0c2dcd68
Steve Hu <stevehu@gmail.com>
2023-11-11 21:34:40 -0500

    fixes #97 refactor ServerConfig and ModuleRegistry

--- stateless-auth/src/main/java/com/networknt/auth/StatelessAuthHandler.java
+++ stateless-auth/src/main/java/com/networknt/auth/StatelessAuthHandler.java
@@ -402,6 +402,6 @@ public class StatelessAuthHandler implements MiddlewareHandler {
 
     @Override
     public void register() {
-        ModuleRegistry.registerModule(StatelessAuthHandler.class.getName(), Config.getInstance().getJsonMapConfigNoCache(StatelessAuthConfig.CONFIG_NAME), null);
+        ModuleRegistry.registerModule(StatelessAuthConfig.CONFIG_NAME, StatelessAuthHandler.class.getName(), Config.getInstance().getJsonMapConfigNoCache(StatelessAuthConfig.CONFIG_NAME), null);
     }
 }
